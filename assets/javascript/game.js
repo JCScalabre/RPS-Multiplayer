480d25689829af1c3bfe04b61532c26c972d37ac
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-02 23:20:13 -0500

    2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,16 +16,72 @@ var playerNumber = 1;
 
 $("#start").on("click", start);
 
+// Start Function:
 function start() {
-  console.log("You started the game");
-  var player = $(".form-control").val().trim();
-  console.log(player);
-  console.log(playerNumber);
-  playerNumber++;
 
-  database.ref().set({
+  var player = $(".form-control").val().trim();
+
+  database.ref("/player" + playerNumber).set({
     player: playerNumber,
     name: player
   })
 
+  $("#player" + playerNumber + "name").html(player);
+
+  $("#playernumber").html("Enter Player 2's Name");
+
+  $(".form-control").val("");
+
+  playerNumber++;
+
+  // If both players have entered their name:
+  if (playerNumber === 3) {
+    $("#inputcard").html('<h4 class="card-title" id="maythe">May the best win!</h4>');
+  }
 }
+
+var p1choice;
+
+var p2choice;
+
+function playerchoices() {
+
+  $("#R1").on("click", function() {
+    database.ref("/player1").update({
+      choice: "Rock"
+    });
+  })
+
+  $("#P1").on("click", function() {
+    database.ref("/player1").update({
+      choice: "Paper"
+    });
+  })
+
+  $("#S1").on("click", function() {
+    database.ref("/player1").update({
+      choice: "Scissors"
+    });
+  })
+
+  $("#R2").on("click", function() {
+    database.ref("/player2").update({
+      choice: "Rock"
+    });
+  })
+
+  $("#P2").on("click", function() {
+    database.ref("/player2").update({
+      choice: "Paper"
+    });
+  })
+
+  $("#S2").on("click", function() {
+    database.ref("/player2").update({
+      choice: "Scissors"
+    });
+  })
+
+};
+
+playerchoices();
